ac5dc6215482ccf752b66f0b6343044ab318bb60
Sad <cougar.l@hotmail.com>
2022-01-24 03:41:16 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-Hooks.on("renderPlayerList", (dialog, $element) => {
-  $element.css("z-index", 9);
+Hooks.on("renderSceneControls", (dialog, $element) => {
+  const html = document.querySelector('.main-controls');
+  html.style.height = '550px';
+  html.style.overflowY = "auto";
+  html.style.pointerEvents = "auto";
 });
