36b606ff4d3968b0142c8cd3d29f06c5b4ba2706
zeteticl <cougar.l@hotmail.com>
2022-01-24 12:31:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 Hooks.on("renderSceneControls", (dialog, $element) => {
   const html = document.querySelector('.main-controls');
-  html.style.height = '550px';
+  html.style.height = '80vh';
   html.style.overflowY = "auto";
   html.style.pointerEvents = "auto";
 });
